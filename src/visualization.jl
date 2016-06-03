71211a9eba4204dd0ea2b840bee0971434c62769
C HO <christopherj.ho@gmail.com>
2016-06-03 14:38:02 -0700

    minor fix on visualization, sloppy pareto curve visualization notebook

--- src/visualization.jl
+++ src/visualization.jl
@@ -122,7 +122,7 @@ function draw_sedan(pp::PhysicalParam, s::CarState, v_nom::Float64, frame::Float
 	annotate("$(round(s.vel,2))",xy=(x_ctr,y_ctr))
 end
 
-function visualize(mdp::MLMDP,
+function visualize(mdp::Union{MLMDP,MLPOMDP},
                    s::MLState, a::MLAction, sp=create_state(mdp); debug::Bool=false, frame::Float64=0., nb_rows::Int=1, two_frame_crash::Bool=false)
 	#Placeholder!
 	clf()
