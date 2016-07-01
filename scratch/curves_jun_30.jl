f69ea1288d2ed8fb07e8d36f1a54d94b6f7c21ec
Zachary Sunberg <sunbergzach@gmail.com>
2016-06-30 22:16:34 -0700

    added scripts for initial condition and curve generation

---
+++ scratch/curves_jun_30.jl
@@ -0,0 +1,74 @@
+using Multilane
+using MCTS
+using GenerativeModels
+using POMDPToolbox
+using JLD
+using POMDPs
+using DataFrames
+using Plots
+
+N=1
+
+filename = "initials_Jun_30_17_27.jld"
+initials = load(filename)
+problems = initials["problems"]
+initial_states = initials["initial_states"]
+
+# Calculate single points
+point_solvers = Dict{UTF8String, Solver}(
+    "random" => RandomSolver(),
+    "heuristic" => SimpleSolver()
+)
+
+point_results = evaluate(Dict(take(problems,1)), initial_states, point_solvers, parallel=true, N=N)
+
+dpws = DPWSolver(depth=20,
+                 n_iterations=500,
+                 exploration_constant=100.0,
+                 k_state=4.0,
+                 alpha_state=1/8,
+                 rollout_solver=SimpleSolver()) 
+
+curve_solvers = Dict{UTF8String, Solver}(
+    "dpw" => dpws,
+    # "robust" => 
+    #     Robust
+    "single_behavior" => 
+        SingleBehaviorSolver(dpws,
+                 IDMMOBILBehavior("normal", 30.0, 10.0, 1))
+)
+
+curve_results = evaluate(problems, initial_states, curve_solvers, parallel=true, N=N)
+
+results = merge_results!(curve_results, point_results)
+
+println(results["stats"])
+
+filename = string("results_", Dates.format(Dates.now(),"u_d_HH_MM"), ".jld")
+results["histories"] = nothing
+save(filename, results)
+println("results saved to $filename")
+
+stats = results["stats"]
+mean_performance = by(stats, :solver_key) do df
+    by(df, :lambda) do df
+        DataFrame(steps_in_lane=mean(df[:steps_in_lane]),
+                  nb_brakes=mean(df[:nb_brakes]),
+                  time_to_lane=mean(df[:time_to_lane]),
+                  steps=mean(df[:steps]),
+                  )
+    end
+end
+
+unicodeplots()
+for g in groupby(mean_performance, :solver_key)
+    if size(g,1) > 1
+        # plot!(g, :steps_in_lane, :nb_brakes, group=:solver_key)
+        plot!(g, :time_to_lane, :nb_brakes, group=:solver_key)
+    else
+        # scatter!(g, :steps_in_lane, :nb_brakes, group=:solver_key)
+        scatter!(g, :time_to_lane, :nb_brakes, group=:solver_key)
+    end
+end
+gui()
+
