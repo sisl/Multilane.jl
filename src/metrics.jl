d41ba1c76817e904894cf18f2c4dd91420a856de
Zachary Sunberg <sunbergzach@gmail.com>
2016-08-20 00:23:28 -0700

    added metrics file

---
+++ src/metrics.jl
@@ -0,0 +1,53 @@
+abstract SimMetric # functions: key(m), datatype(m), calculate(m, mdp, sim)
+
+type MaxBrakeMetric <: SimMetric end
+key(::MaxBrakeMetric) = :max_brake
+datatype(::MaxBrakeMetric) = Float64
+function calculate(m::MaxBrakeMetric, problem::NoCrashProblem, sim::HistoryRecorder)
+    sh = sim.state_hist
+    max_brake = 0.0
+    for i in 1:length(sh)-1
+        mbi = max_braking(problem, sh[i], sh[i+1])
+        if mbi > max_brake
+            max_brake = mbi
+        end
+    end
+    return max_brake
+end
+
+type NumBehaviorBrakesMetric
+    idx::Int
+    name::AbstractString
+    threshold::Float64
+end
+key(m::NumBehaviorBrakesMetric) = Symbol(string(m.name, "_nb_brakes"))
+datatype(::NumBehaviorBrakesMetric) = Int
+function calculate(m::NumBehaviorBrakesMetric, problem::NoCrashProblem, sim::HistoryRecorder)
+    sh = sim.state_hist
+    nb_brakes = 0
+    for i in 1:length(sh)-1
+        nb_leaving = 0 
+        dt = problem.dmodel.phys_param.dt
+        s = sh[i]
+        sp = sh[i+1]
+        for (i,c) in enumerate(s.env_cars)
+            if length(sp.env_cars) >= i-nb_leaving
+                cp = sp.env_cars[i-nb_leaving]
+            else
+                break
+            end
+            if cp.id != c.id
+                nb_leaving += 1
+                continue
+            else
+                if !isnull(c.behavior) && get(c.behavior).idx == m.idx
+                    acc = (cp.vel-c.vel)/dt
+                    if acc < -m.threshold 
+                        nb_brakes+=1
+                    end
+                end
+            end
+        end
+    end
+    return nb_brakes
+end
