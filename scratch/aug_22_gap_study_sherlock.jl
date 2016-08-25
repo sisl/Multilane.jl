272a80be17b57e9640f523c6ecb32c26bf8219f9
Zachary Sunberg <sunbergzach@gmail.com>
2016-08-24 21:59:32 -0700

    ready to run gap study on sherlock

--- scratch/aug_22_gap_study_sherlock.jl
+++ scratch/aug_22_gap_study_sherlock.jl
@@ -3,6 +3,13 @@ using StatsBase
 using MCTS
 using JLD
 
+behaviors=Dict{UTF8String,Any}()
+for p in linspace(0., 3/4, 4)
+    wv = (1-p)/8*ones(9)
+    wv[1] = p
+    behaviors[@sprintf("agents_%03d", 100*p)] =
+            DiscreteBehaviorSet(Multilane.NINE_BEHAVIORS, WeightVec(wv))
+end
 
 dpws = DPWSolver(depth=20,
                  n_iterations=500,
@@ -12,13 +19,13 @@ dpws = DPWSolver(depth=20,
                  rollout_solver=SimpleSolver()) 
 solvers = Dict{UTF8String, Any}(
     "dpw"=>dpws,
-    "assume_normal"=>SingleBehaviorSolver(dpws, normal)
+    "assume_normal"=>SingleBehaviorSolver(dpws, Multilane.NORMAL)
 )
 
-curve = TestSet(lambda=[0.1, 1.0, 2.15, 4.64, 10., 50.,], N=500)
+curve = TestSet(lambda=[0.1, 1.0, 2.15, 4.64, 10., 50.,], N=1)
 
 tests = []
-for p in linspace(0., 1., 5)
+for p in linspace(0., 3/4, 4)
     push!(tests, TestSet(curve, solver_key="dpw",
                          behaviors=@sprintf("agents_%03d", p*100),
                          key=@sprintf("upper_bound_%03d", p*100))) 
@@ -35,9 +42,15 @@ for is in values(objects["initial_states"])
     @assert !isnull(is.env_cars[1].behavior)
 end
 
-results = evaluate(tests, objects, parallel=true)
+sbatch_spawn(tests, objects,
+             batch_size=6,
+             time_per_batch="1:00:00",
+             submit_command="bash",
+             template_name="theresa.sh")
 
-filename = string("results_", Dates.format(Dates.now(),"u_d_HH_MM"), ".jld")
-results["histories"] = nothing
-save(filename, results)
-println("results saved to $filename")
+# results = evaluate(tests, objects, parallel=true)
+# 
+# filename = string("results_", Dates.format(Dates.now(),"u_d_HH_MM"), ".jld")
+# results["histories"] = nothing
+# save(filename, results)
+# println("results saved to $filename")
