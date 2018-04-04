792d4dcb6b00d127557f7ad7f33b42b4e3d269cf
Zachary Sunberg <sunbergzach@gmail.com>
2018-04-04 14:08:15 -0700

    journal

--- scratch_18_04/allcurves.jl
+++ scratch_18_04/allcurves.jl
@@ -13,6 +13,7 @@ end
 # filename = Pkg.dir("Multilane", "data/baseline_curve_Sunday_1_Apr_02_18.csv")
 # filename = Pkg.dir("Multilane", "data/baseline_curve_Monday_2_Apr_14_49.csv")
 filename = Pkg.dir("Multilane", "data/all_gaps_Monday_2_Apr_21_34.csv")
+# filename = Pkg.dir("Multilane", "data/all_gaps_Wednesday_4_Apr_04_20.csv")
 
 data = CSV.read(filename, nullable=true)
 
