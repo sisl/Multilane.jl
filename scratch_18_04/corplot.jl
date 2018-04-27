23ca019c443562e01264ca1a9b6865f7dfbd5cfa
Zachary Sunberg <sunbergzach@gmail.com>
2018-04-27 11:26:17 -0700

    corplot updated

--- scratch_18_04/corplot.jl
+++ scratch_18_04/corplot.jl
@@ -12,12 +12,19 @@ end
 
 # filename = Pkg.dir("Multilane", "data/cor_trend_Monday_2_Apr_19_49.csv")
 # filename = Pkg.dir("Multilane", "data/cor_trend_Tuesday_3_Apr_07_20.csv")
-filename = Pkg.dir("Multilane", "data/cor_trend_Tuesday_3_Apr_20_35.csv")
+# filename = Pkg.dir("Multilane", "data/cor_trend_Tuesday_3_Apr_20_35.csv")
+filename = Pkg.dir("Multilane", "data/cor_trend_Friday_27_Apr_03_39.csv")
 
-data = CSV.read(filename, nullable=true)
+data = CSV.read(filename)
 
 data[:time_to_lane] = data[:steps_to_lane].*data[:dt]
 data[:brakes_per_step] = data[:nb_brakes]./data[:n_steps]
+
+data = @from i in data begin
+    @where i.solver != "outcome"
+    @select i
+    @collect DataFrame
+end
 
 combined = by(data, [:cor, :solver]) do df
     return DataFrame(reached_lane=sum(df[:terminal].=="lane")/nrow(df),
@@ -32,7 +39,7 @@ for df in groupby(combined, [:solver])
     df = sort(df, cols=[:cor])
     name = only(unique(df[:solver]))
     # 95% confidence region from Hoeffding Bound
-    confidence = 0.95
+    confidence = 0.5
     n = only(unique(df[:n]))
     confidence_radius = sqrt(log((1.0-confidence)/2)/(-2*n))
     plt = Plots.Linear(convert(Vector{Float64}, df[:cor]),
